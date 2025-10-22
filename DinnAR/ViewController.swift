3f32f9f2e4b241eb71dac2dee6c621add72758c2
AnGe4601 <angelaliu239@gmail.com>
2025-10-22 13:23:56 -0500

    Push preferences screens

--- DinnAR/ViewController.swift
+++ DinnAR/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var likeLabel: UILabel!
+    @IBOutlet weak var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        nextButton.layer.cornerRadius = 10
+        nextButton.backgroundColor = UIColor(named: "ButtonColor")
+        likeLabel.font = UIFont(name: "DMSerifText-Italic", size: 40)
     }
 
-
+    @IBAction func nextButtonPressed(_ sender: Any) {
+        performSegue(withIdentifier: "ToCuisineVCSI", sender: self)
+    }
+    
 }
 
